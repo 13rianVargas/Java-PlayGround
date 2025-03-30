972d7d3e308c933505d6fa85ee406d068557e6b8
13rian Vargas <bsvargasc13@gmail.com>
2025-03-29 21:45:36 -0500

    git init

---
+++ README.md
@@ -0,0 +1,1 @@
+<h1> Hello 13rian Vargas </h1>
---
+++ src/App.java
@@ -0,0 +1,26 @@
+public class App {
+    public static void main(String[] args) {
+
+        var searchMe = "Look for a substring in me";
+        var substring = "sub";
+        var foundIt = false;
+
+        var max = searchMe.length() -
+                  substring.length();
+
+    test:
+        for (var i = 0; i <= max; i++) {
+            var n = substring.length();
+            var j = i;
+            var k = 0;
+            while (n-- != 0) {
+                if (searchMe.charAt(j++) != substring.charAt(k++)) {
+                    continue test;
+                }
+            }
+            foundIt = true;
+                break test;
+        }
+        System.out.println(foundIt ? "Found it" : "Didn't find it");
+    }
+}
